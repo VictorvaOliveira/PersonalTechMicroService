57d4966ac70cfbdf805778a5e2d2de262f849404
Victor Vinicius <victorvaoliveira@gmail.com>
2019-09-04 13:40:10 -0300

    Gerando token

--- restapi/src/main/java/personaltech/ejb/PersonalTrainerBean.java
+++ restapi/src/main/java/personaltech/ejb/PersonalTrainerBean.java
@@ -15,38 +15,47 @@ public class PersonalTrainerBean {
 
 	@PersistenceContext(unitName = "pu")
 	private EntityManager entityManager;
-	
+
 	@PostConstruct
 	private void initializeBean() {
-		
+
 	}
-	
-	public List<PersonalTrainer> getPersonalTrainers(){
-		
+
+	public List<PersonalTrainer> getPersonalTrainers() {
+
 		String jpql = ("select pt from PersonalTrainer pt");
 		Query query = entityManager.createQuery(jpql, PersonalTrainer.class);
 		List<PersonalTrainer> personaltrainer = query.getResultList();
-		
+
 		if (personaltrainer != null) {
 			return personaltrainer;
 		}
 		return null;
 	}
-	
+
 	public PersonalTrainer getOnePersonal(int id) {
 		PersonalTrainer personal = entityManager.find(PersonalTrainer.class, id);
-		
-		if(personal != null)
+
+		if (personal != null)
 			return personal;
 		return null;
 	}
-	
+
 	public PersonalTrainer cadastrarPersonal(String name, String email, String senha) {
 		PersonalTrainer personal = new PersonalTrainer();
 		personal.setNome(name);
 		personal.setEmail(email);
 		personal.setSenha(senha);
 		entityManager.persist(personal);
+		return personal;
+	}
+
+	public PersonalTrainer login(String login, String senha) {
+		String jpql = ("select pt from PersonalTrainer pt where pt.email = :pLogin and pt.senha= :pSenha");
+		Query query = entityManager.createQuery(jpql, PersonalTrainer.class);
+		query.setParameter("pLogin", login);
+		query.setParameter("pSenha", senha);
+		PersonalTrainer personal = (PersonalTrainer) query.getSingleResult();
 		return personal;
 	}
 }
--- restapi/src/main/java/personaltech/restapi/PersonalTechService.java
+++ restapi/src/main/java/personaltech/restapi/PersonalTechService.java
@@ -1,26 +1,29 @@
 package personaltech.restapi;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;	
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
+import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
-//import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-//import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-//import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
 import personaltech.entidades.PersonalTrainer;
+import personaltech.jwtConfiguration.JsonTokenNeeded;
+import personaltech.util.JwTokenHelper;
 import personaltech.ejb.*;
 
 @Path("/personal")
@@ -28,21 +31,42 @@ import personaltech.ejb.*;
 @Consumes(APPLICATION_JSON)
 @Transactional
 public class PersonalTechService {
-	
+
 	@EJB
 	private PersonalTrainerBean personalTrainerBean;
-	
+
+	/*
+	 * Login personal trainer
+	 */
+	@POST
+	@Path("/login")
+	@Consumes(APPLICATION_FORM_URLENCODED)
+	public Response login(@FormParam("login") String login, @FormParam("senha") String senha) {
+
+		PersonalTrainer personal = personalTrainerBean.login(login, senha);
+
+		String token = null;
+
+		if (personal != null) {
+			token = JwTokenHelper.getInstance().generateToken(login, senha);
+			return Response.ok(personal).header(AUTHORIZATION, "Bearer" + token).build();
+		}
+		return Response.status(NOT_FOUND).build();
+
+	}
+
 	/*
 	 * Recupera uma lista de Personal Trainer
 	 */
 	@GET
+	@JsonTokenNeeded
 	public Response findAllPersonalTrainer() {
 		List<PersonalTrainer> allPersonal = personalTrainerBean.getPersonalTrainers();
-		if (allPersonal != null) 
+		if (allPersonal != null)
 			return Response.ok(allPersonal).build();
 		return Response.status(NOT_FOUND).build();
 	}
-	
+
 	/*
 	 * Recupera somente um Personal Trainer
 	 */
@@ -54,20 +78,21 @@ public class PersonalTechService {
 			return Response.ok(personal).build();
 		return Response.status(NOT_FOUND).build();
 	}
-	
+
 	/*
 	 * Persiste no banco de dados um objeto Personal trainer
 	 */
 	@POST
 	@Path("/newpersonal")
 	@Consumes(APPLICATION_FORM_URLENCODED)
-	public Response persistPersonal(@FormParam("name") String name, @FormParam("email") String email, @FormParam("senha") String senha) {
+	public Response persistPersonal(@FormParam("name") String name, @FormParam("email") String email,
+			@FormParam("senha") String senha) {
 		PersonalTrainer personal = personalTrainerBean.cadastrarPersonal(name, email, senha);
-		
+
 		if (personal != null) {
 			return Response.ok(personal).build();
 		}
 		return Response.status(500).build();
 	}
-	
+
 }
